c17f92738cbad0727b8a9afa9a539f79605812f1
james.bray <dksc105248@dcsg.com>
2020-04-16 15:36:46 -0500

    first commit

---
+++ src/main/java/com/jrbrayjr/sample/genomeanalyzer/GenomeAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jrbrayjr.sample.genomeanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomeAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenomeAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrbrayjr/sample/genomeanalyzer/GenomeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrbrayjr.sample.genomeanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
